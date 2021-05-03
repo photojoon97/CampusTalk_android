7ad051e2a702789d777eb9770a48b66cbba59c11
matthewsmkim <forme3328@naver.com>
2021-05-03 20:32:45 +0900

    메인화면 구성

--- app/src/main/java/com/example/campustalk/MainActivity.java
+++ app/src/main/java/com/example/campustalk/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.campustalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEmail;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmail = findViewById(R.id.login_email);
+        mPassword = findViewById(R.id.login_password);
+
+        findViewById(R.id.login_signup).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.login_success).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.sign_logout).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.sign_find).setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
